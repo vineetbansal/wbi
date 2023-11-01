e2d2061fb19ac3aa5c30767eb13e041c38ef0058
Vineet Bansal <vineetbansal@protonmail.com>
2023-11-01 15:58:33 -0400

    sample commands for wbi

---
+++ src/wbi/__main__.py
@@ -0,0 +1,34 @@
+from wbi.commands import hello
+from wbi.commands import goodbye
+
+
+def main():
+    import argparse
+    import os
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    import wbi
+
+    parser.add_argument("--version", action="version", version="wbi " + wbi.__version__)
+
+    modules = (hello, goodbye)
+
+    subparsers = parser.add_subparsers(title="Choose a command")
+    subparsers.required = True
+
+    def get_str_name(module):
+        return os.path.splitext(os.path.basename(module.__file__))[0]
+
+    for module in modules:
+        this_parser = subparsers.add_parser(
+            get_str_name(module), description=module.__doc__
+        )
+        module.add_args(this_parser)
+        this_parser.set_defaults(func=module.main)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
