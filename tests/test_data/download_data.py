091abd85011aca3f039d2eac389f68697790bb9b
Vineet Bansal <vineetbansal@protonmail.com>
2023-12-14 15:24:24 -0500

    added matplotlib as dep

---
+++ tests/test_data/download_data.py
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# best_model.h5
+gdown 1byBD4xWGrKBUY3VLcOl7JPhBntooiJ1t
--- tests/test_data/download_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget --no-check-certificate 'https://drive.google.com/file/d/1jTZ8rWeHWAUGgmo5xlWhSWgnq6iP-aPy/view?usp=drive_link' -O eigenWorms.mat
-wget --no-check-certificate 'https://drive.google.com/file/d/1byBD4xWGrKBUY3VLcOl7JPhBntooiJ1t/view?usp=drive_link' -O best_model.h5
