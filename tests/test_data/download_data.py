de7cd0158fd3b0ca7506603549e13de730fe94da
Vineet Bansal <vineetbansal@protonmail.com>
2023-12-14 15:52:48 -0500

    Download script for test data

--- tests/test_data/download_data.py
+++ tests/test_data/download_data.py
@@ -1,4 +1,12 @@
-#!/bin/bash
+import os.path
+import urllib.request
 
-# best_model.h5
-gdown 1byBD4xWGrKBUY3VLcOl7JPhBntooiJ1t
+this_dir = os.path.dirname(__file__)
+
+urls_to_files = {
+    "https://dl.dropboxusercontent.com/scl/fi/jphaqhoci2fae0tmy0gna/best_model.h5?rlkey=w7tbd1woo8yafzq1m1djlf2um&dl=1": "best_model.h5"
+}
+
+for url, file in urls_to_files.items():
+    print(f"Downloading {file}")
+    urllib.request.urlretrieve(url, f"{this_dir}/{file}")
