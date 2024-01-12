92ce82200058eeb7ab9aab3cb1786ae954ad807b
anushka255 <aa9078@princeton.edu>
2024-01-12 15:37:14 -0500

    Updated conftest to download files from dropbox

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import os.path
 import pytest
-from wbi.data.ondemand import CACHE_PATH
+from wbi.data.ondemand import get_file
 
 
 @pytest.fixture(scope="session")
@@ -10,4 +10,12 @@ def data_folder():
 
 @pytest.fixture(scope="session")
 def expensive_data_folder():
-    return os.path.join(CACHE_PATH, "bs")
+    files = [
+        "bs/sCMOS_Frames_U16_1024x512.dat",
+        "bs/framesDetails.txt",
+        "bs/other-frameSynchronous.txt",
+        "bs/other-volumeMetadataUtilities.txt",
+        "bs/LowMagBrain20231024_153442",
+    ]
+    data_folder = os.path.dirname(list(map(get_file, files))[0])
+    return data_folder
