3e6fbb06929e165e965ceda1e0c3c5fa87306c75
anushka255 <aa9078@princeton.edu>
2024-01-22 23:46:31 -0500

    Converted list to a dict

--- src/wbi/alignment.py
+++ src/wbi/alignment.py
@@ -14,15 +14,16 @@ class Alignment(File):
             names: alignments_data[names][0, 0].dtype.names
             for names in alignments_data.dtype.names
         }
-        self.lowResFluor2BF = [
-            alignments_data["lowResFluor2BF"][0, 0][val][0, 0]
-            for val in dtypes["lowResFluor2BF"]
-        ]
-        self.S2AHiRes = [
-            alignments_data["S2AHiRes"][0, 0][val][0, 0] for val in dtypes["S2AHiRes"]
-        ]
-        self.Hi2LowResF = [
-            alignments_data["Hi2LowResF"][0, 0][val][0, 0]
-            for val in dtypes["Hi2LowResF"]
-        ]
+        self.lowResFluor2BF = {
+            label: alignments_data["lowResFluor2BF"][0, 0][label][0, 0]
+            for label in dtypes["lowResFluor2BF"]
+        }
+        self.S2AHiRes = {
+            label: alignments_data["S2AHiRes"][0, 0][label][0, 0]
+            for label in dtypes["S2AHiRes"]
+        }
+        self.Hi2LowResF = {
+            label: alignments_data["Hi2LowResF"][0, 0][label][0, 0]
+            for label in dtypes["Hi2LowResF"]
+        }
         self.background = alignments_data["background"][0, 0]
