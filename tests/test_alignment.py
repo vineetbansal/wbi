9bad83e11429fbaa2f35c18c3f2802fde9ed4f2d
anushka255 <aa9078@princeton.edu>
2024-01-23 20:34:43 -0500

    Refactored test

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -3,7 +3,7 @@ from wbi.alignment import Alignment
 
 def test_alignment(data_folder):
     alignment = Alignment(data_folder)
-    assert len(alignment.lowResFluor2BF) == 4
-    assert len(alignment.S2AHiRes) == 6
-    assert len(alignment.Hi2LowResF) == 4
-    assert alignment.background.shape == (1024, 512)
+    assert len(alignment["lowResFluor2BF"]) == 4
+    assert len(alignment["S2AHiRes"]) == 6
+    assert len(alignment["Hi2LowResF"]) == 4
+    assert alignment["background"].shape == (1024, 512)
